ccbb0fd967cd27af6e06db603de21fb0e5948e17
Kaila Kay <kekay0528@gmail.com>
2020-05-31 20:45:30 -0400

    5.31

--- functions.py
+++ functions.py
@@ -1,1 +1,17 @@
 import locale
+
+def get_binary_column(dfName, colName, qual_for_false):
+    """
+    Iterate through a column to append an additional binary (T/F) column to a DataFrame
+    
+    dfName = Name of Pandas DataFrame to pull information from
+    colName = Name of column from DataFrame to pull information from
+    qual_for_false = condition where value is <= qual_for_false returns a binary 0, else 1
+    """
+    newCol = []
+    for val in dfName[colName]:
+        if val <= qual_for_false:
+            newCol.append('0')
+        else:
+            newCol.append('1')
+    return newCol
